26dffc753420f255552328f8241454a328400062
aman verma <aman.verma@oniondev.com>
2025-02-24 17:43:58 +0530

    django project initialized

---
+++ core_stack_backend_onprem/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for core_stack_backend_onprem project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core_stack_backend_onprem.settings")
+
+application = get_wsgi_application()
